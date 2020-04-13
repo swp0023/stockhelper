f2050c2919cb6e5bded61020b44ae5348968ab2e
swpark <wooloosa@kakao.com>
2020-04-13 00:39:00 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+ 
+@app.route('/')
+def hello_world():
+  return '주말 잘 보내세요\n우혁프로님, 남기경 대리님, 오건철 대리님'
+ 
+if __name__ == '__main__':
+  app.run('0.0.0.0', port=80)
