d581af9c5eff2d5e798087c743b3b445d836d2db
whchoiv <arigato1224@gmail.com>
2020-04-15 21:33:10 +0900

    test add adminpage

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ def hello_world():
   return '주말 잘 보내세요\n우혁프로님, 남기경 대리님, 오건철 대리님'
  
 @app.route('/admin')
-def hello_world2():
-  return 'Out!!!'
+def adminPage():
+  return "Out!!!!"
 
 if __name__ == '__main__':
   app.run('0.0.0.0', port=5000)
