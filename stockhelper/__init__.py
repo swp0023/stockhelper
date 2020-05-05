44eb96c50029f3940a9c9968fd3d19ef3c553439
swpark <psw607me@gmail.com>
2020-05-05 11:51:42 +0900

    add api blueprint

--- stockhelper/__init__.py
+++ stockhelper/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 
+from stockhelper.api.v1.account import api_v1_account
 
 app = Flask(__name__, static_url_path='')
+
+app.register_blueprint(api_v1_account, url_prefix='/api/v1/account')
 
  
 @app.route('/')
---
+++ stockhelper/api/v1/account.py
@@ -0,0 +1,4 @@
+from flask import Blueprint, jsonify
+
+
+api_v1_account = Blueprint('api_v1_account', __name__)
