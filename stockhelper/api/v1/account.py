09278232cd2bc99bbdc5f46cb2f8613814f35cb5
swpark <psw607me@gmail.com>
2020-06-01 21:37:23 +0900

    use login_log when login or create user

--- stockhelper/api/v1/account.py
+++ stockhelper/api/v1/account.py
@@ -1,35 +1,41 @@
 from flask import Blueprint, jsonify, request
+from werkzeug.security import generate_password_hash, check_password_hash
 from stockhelper.config import RESPONSE_MSG_200, RESPONSE_MSG_400
 from stockhelper.database import db_session
 
-from stockhelper.models import ACCOUNT
+from stockhelper.models import ACCOUNT, LOGIN_LOG
 
 
 api_v1_account = Blueprint('api_v1_account', __name__)
 
 
+def insert_into_login_log(account_id, ip):
+    log = LOGIN_LOG(
+        account_id=account_id,
+        ip=ip)
+    db_session.add(log)
+    db_session.commit()
+    # db_session.flush()
+    
+
 @api_v1_account.route('/login', methods = ['POST'])
 def login():
-    # need password hash check
     username = request.json.get('username')
     password = request.json.get('password')
+    ip = request.json.get('ip')
 
     if not username or not password:
         return jsonify(code=400, msg=RESPONSE_MSG_400), 400
 
     user = db_session.query(ACCOUNT).filter(ACCOUNT.username == username).first()
     
-    if user is not None and user.password == password:
+    if user is not None and check_password_hash(user.password, password):
+        # login_user(user, remember=True)    
+        # if fcm_token is not None:
+        # user.fcm_token = fcm_token
+        # session.commit()
+        insert_into_login_log(user.id, ip)
         return jsonify(code=200, msg=RESPONSE_MSG_200)
-    # if user.status != AccountStatus.ACTIVATED:
-    #     return jsonify(code=403, msg='활성화되지 않은 계정입니다. 관리자에게 문의해주세요'), 403
-    # if user is not None and check_password_hash(user.password, password):
-    #     login_user(user, remember=True)
-    #     if fcm_token is not None:
-    #         user.fcm_token = fcm_token
-    #         session.commit()
-        # print(user.id, user.username, user.password)
-        # return jsonify(code=200, msg=RESPONSE_MSG_200)
     return jsonify(code=403, msg='계정이 존재하지 않거나, 비밀번호가 일치하지 않습니다.'), 403
 
 
@@ -37,8 +43,10 @@ def login():
 def register():
     username = request.json.get('username')
     password = request.json.get('password')
+    ip = request.json.get('ip')
+    email = request.json.get('email')
 
-    if not username or not password:
+    if not username or not password or not ip or not email:
         return jsonify(code=400, msg=RESPONSE_MSG_400), 400
 
     user = db_session.query(ACCOUNT).filter(ACCOUNT.username == username).first()
@@ -46,9 +54,14 @@ def register():
     if user is not None:
         return jsonify(code=403, msg='Need other username'), 403
 
-    new_user = ACCOUNT(username=username, password=password)
+    new_user = ACCOUNT(
+        username=username, 
+        password=generate_password_hash(password),
+        email=email)
     db_session.add(new_user)
     db_session.commit()
     db_session.flush()
+
+    insert_into_login_log(new_user.id, ip)
 
     return jsonify(code=200)
--- stockhelper/models.py
+++ stockhelper/models.py
@@ -1,19 +1,22 @@
-from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, ForeignKey, DateTime
 from stockhelper.database import Base
+from sqlalchemy.sql import func
+
 
 class ACCOUNT(Base):
     __tablename__ = 'account'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String(20), unique=True, nullable=False)
-    password = Column(String(20), nullable=False)
+    password = Column(String(100), nullable=False)
     email = Column(String(40), unique=True)
     email_cert = Column(Boolean, default=False)
     admin = Column(Boolean, default=False)
 
-    def __init__(self, username=None, password=None):
+    def __init__(self, username=None, password=None, email=None):
         self.username = username
         self.password = password
+        self.email = email
 
     def to_dict(self):
         return {
@@ -22,10 +25,15 @@ class ACCOUNT(Base):
             'password': self.password
         }
 
+
 class LOGIN_LOG(Base):
     __tablename__ = 'login_log'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     account_id = Column(Integer, ForeignKey('account.id'), nullable=False)
-    login_datetime = Column(TIMESTAMP)
-    login_ip = Column(String(15))
+    datetime = Column(DateTime(timezone=True), default=func.now())
+    ip = Column(String(15), default='0.0.0.0')
+
+    def __init__(self, account_id=None, ip=None):
+        self.account_id = account_id
+        self.ip = ip
